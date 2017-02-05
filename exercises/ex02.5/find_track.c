5737fe47a85cef631dd350df5d10fe8c641a486b
Chris Wallace <christopher.wallace@students.olin.edu>
2017-02-05 15:29:45 -0500

    COmpleted exercise 2.5

--- exercises/ex02.5/find_track.c
+++ exercises/ex02.5/find_track.c
@@ -7,6 +7,8 @@ Modified version of an example from Chapter 2.5 of Head First C.
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <regex.h>        
+
 
 #define NUM_TRACKS 5
 
@@ -37,7 +39,25 @@ void find_track(char search_for[])
 // Prints track number and title.
 void find_track_regex(char pattern[])
 {
+    regex_t regex;
+    int regStatus;
+    int i;
     // TODO: fill this in
+    regStatus = regcomp(&regex, pattern, 0);
+    if (regStatus) {
+        fprintf(stderr, "Error compiling regex\n");
+        exit(1);
+    }
+
+    /* Execute regular expression */
+    for (i=0; i<NUM_TRACKS; i++) {
+        regStatus = regexec(&regex, tracks[i], 0, NULL, 0);
+        if (!regStatus) {
+            printf("Track %i: '%s'\n", i, tracks[i]);
+        }
+    }
+    regfree(&regex);
+    
 }
 
 // Truncates the string at the first newline, if there is one.
@@ -58,7 +78,7 @@ int main (int argc, char *argv[])
     fgets(search_for, 80, stdin);
     rstrip(search_for);
 
-    find_track(search_for);
+    find_track_regex(search_for);
     //find_track_regex(search_for);
 
     return 0;
